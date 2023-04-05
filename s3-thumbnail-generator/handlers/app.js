357cd2508b4ea0df2577d709cd8a19b09a953149
Aadityakiran S <aadityakirans@gmail.com>
2023-04-05 11:27:28 +0530

    Reworked code to be more modular and used aws-multipart-parser to get to implement parsing from form-data

--- s3-thumbnail-generator/handlers/app.js
+++ s3-thumbnail-generator/handlers/app.js
@@ -1,118 +1,51 @@
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-// const sharp = require('sharp');
-const multer = require('multer');
-const upload = multer({
-    limits: {
-        fileSize: 5 * 1024 * 1024 // 5MB
-    },
-    encoding: 'binary' // Specify encoding used to encode the input data
-});
+const { parse } = require('aws-multipart-parser');
 
-let response;
-
-// exports.generateThumbnail = async (event, context) => {
-//     const bucketName = event.Records[0].s3.bucket.name;  // #TOASK: You could get this from env variables right? How do you do that? Is that the reccomended way?
-//     const objectKey = event.Records[0].s3.object.key;
-//     const thumbnailKey = `${objectKey.split(".")[0]}_thumbnail.jpg`;
-
-//     // Download the original image from S3
-//     let originalImage;
-//     try {
-//         originalImage = await s3.getObject({
-//             Bucket: bucketName,
-//             Key: objectKey
-//         })
-//     } catch (error) { // #TOASK: Some way to prevent myself from writing this try-catch all the time? I think asyncWrapper right? But I didn't understand that properly. Could you explain?
-//         response = {
-//             statusCode: 500,
-//             body: error.message,
-//         };
-//         return response;
-//     }
-
-//     // #TOASK: I could break the below stuff down into a function but then how do I sure that I return from that and this at the same time if some error occurs?
-//     // Process the image using Sharp
-//     let thumbnailImage;
-//     try {
-//         //Processing once to change aspect ratio
-//         thumbnailImage = await sharp(originalImage)
-//             // #TOASK: Is setting the width and height to hardcoded values like this the right thing to do?
-//             .resize({
-//                 width: 1000, // set the width to 1000 pixels
-//                 height: 500, // set the height to half of the width (1:2 aspect ratio)
-//                 fit: 'fill', // specify how to fit the image in case it doesn't have the exact aspect ratio
-//                 position: 'center' // specify where to position the image if there's any whitespace
-//             })
-//             .toFormat("jpeg", { mozjpeg: true })
-//             .toBuffer();
-
-//         // Reduce the quality setting until the output buffer is below 10KB
-//         let outputQuality = 100;
-//         while (thumbnailImage.length > 30000 && outputQuality >= 10) {
-//             thumbnailImage = await sharp(originalImage)
-//                 .toFormat("jpeg", { mozjpeg: true })
-//                 .jpeg({ quality: outputQuality })
-//                 .toBuffer();
-//             outputQuality -= 10;
-//         }
-//         // #TOASK: How do we prevent larger files from being uploaded like we did earlier? Using form-data can we upload to S3? I think we should be able to.
-//         //Throwing an error if after proccessing still image is large
-//         if (thumbnailImage.length > 30000) {
-//             response = {
-//                 statusCode: 500,
-//                 body: `Can't reduce size even after appreciable quality drop. Please upload a smaller input image`,
-//             };
-//         }
-//     } catch (error) {
-//         response = {
-//             statusCode: 500,
-//             body: error.message,
-//         };
-//         return response;
-//     }
-
-//     // Upload the thumbnail to S3
-//     let putToS3Response;
-//     try {
-//         putToS3Response = await s3.putObject({
-//             Bucket: bucketName,
-//             Key: thumbnailKey,
-//             Body: thumbnailImage,
-//             ContentType: 'image/jpeg'
-//         });
-//     } catch (error) {
-//         response = {
-//             statusCode: 500,
-//             body: error.message,
-//         };
-
-//         return response;
-//     }
-
-//     // Return a success response
-//     const response = {
-//         statusCode: 200,
-//         message: 'Thumbnail generated and uploaded successfully',
-//         body: putToS3Response
-//     };
-//     return response;
-// };
+const BUCKET_NAME = process.env.BUCKET_NAME;
+const { processImageToThumbnail } = require('./helpers/thumbnail-processing-helper.js');
 
 exports.uploadImageAndProcessThumbnail = async (event, context) => {
-    const { body } = event;
-    const { image } = await upload.single('imageBinary')(event['body-json']);
+    const { imageBinary, imageKey } = parse(event, true);
+    const formData = parse(event, true);
+    const thumbnailKey = `${imageKey.split(".")[0]}_thumbnail.jpg`;
 
-    const bucket = process.env.BUCKET_NAME;
-
-    console.log(`Event: ${event}`);
-
-    const response = {
+    let response = {
         statusCode: 200,
-        message: `Event : ${event}, Image bin: ${image.buffer}, Bucket Name: ${bucket}`,
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
     };
 
-    return response;
+    try {
+        let thumbnailImage = await processImageToThumbnail(imageBinary);
 
-    // console.log(`Image Key: ${body.imageKey} Image Path: ${body.file.path}`);
+        let promises = [];
+        promises.push(uploadFileToS3(imageKey, imageBinary));
+        promises.push(uploadFileToS3(thumbnailKey, thumbnailImage));
+
+        await Promise.all(promises); //Uploading both thumbnail and original image
+
+        response.body = `Successfully uploaded ${imageKey} and corresponding thumbnail`;
+        return response;
+
+    } catch (error) {
+        console.log(error);
+        response.body = JSON.stringify(error);
+        return response;
+    }
 }
+
+//#region Private functions
+function uploadFileToS3(filename, fileBinary) {
+    const params = {
+        Bucket: BUCKET_NAME,
+        Key: filename,
+        Body: fileBinary,
+        ContentType: 'image/jpeg'
+    };
+
+    return s3.putObject(params).promise();
+}
+//#endregion
---
+++ s3-thumbnail-generator/handlers/helpers/thumbnail-processing-helper.js
@@ -0,0 +1,35 @@
+const sharp = require('sharp');
+const maxSize = 30000;
+
+// Process the image using Sharp
+const processImageToThumbnail = async (buffer) => {
+    try {
+
+        //Processing once to change aspect ratio
+        let thumbnailImage = await sharp(buffer)
+            .resize({
+                width: 1000, // set the width to 1000 pixels
+                height: 500, // set the height to half of the width (1:2 aspect ratio)
+                fit: 'fill', // specify how to fit the image in case it doesn't have the exact aspect ratio
+                position: 'center' // specify where to position the image if there's any whitespace
+            })
+            .toFormat("jpeg", { mozjpeg: true })
+            .toBuffer();
+
+        // Reduce the quality setting until the output buffer is below 10KB
+        let outputQuality = 90;
+        while (thumbnailImage.length > maxSize && outputQuality >= 10) {
+            thumbnailImage = await sharp(buffer)
+                .toFormat("jpeg", { mozjpeg: true })
+                .jpeg({ quality: outputQuality })
+                .toBuffer();
+            outputQuality -= 10;
+        }
+
+        return thumbnailImage;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+module.exports = { processImageToThumbnail };
